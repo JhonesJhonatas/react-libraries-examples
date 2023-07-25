88338c5111b2b7b76737a0752dbcd0162e5b4208
Jhones Jhontas <jhones.jhonatas@outlook.com.br>
2023-07-25 13:25:50 -0300

    Feat/React-hook-form Page Structure :art:

--- src/pages/ReactHookForm/index.tsx
+++ src/pages/ReactHookForm/index.tsx
@@ -1,3 +1,36 @@
-export function ReactHookForm(){
-  return <div>ReactHookForm</div>
+import { Link } from "@phosphor-icons/react";
+import { ExampleContainer, LinkArea } from "./styled";
+
+export function ReactHookForm() {
+  return (
+    <main>
+      <h1>React-Hook-Form</h1>
+      <LinkArea>
+        <Link />
+        <a href="https://www.react-hook-form.com/">Documentação</a>
+      </LinkArea>
+      <ExampleContainer>
+        <div>
+          <h3>Formulário</h3>
+
+          <form>
+            <input type="text" placeholder="Primeiro Nome" />
+            <input type="text" placeholder="Segundo Nome" />
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Telefone" />
+            <button type="submit" >Submeter</button>
+          </form>
+        </div>
+        <div>
+          <h3>Watch</h3>
+        </div>
+        <div>
+          <h3>Errors</h3>
+        </div>
+        <div>
+          <h3>Submit</h3>
+        </div>
+      </ExampleContainer>
+    </main>
+  );
 }
